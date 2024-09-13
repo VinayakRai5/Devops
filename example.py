ccd2077568fd62327a3ae08556e99dcf98d6195a
Rohanchidri <160914626+Rohanchidri@users.noreply.github.com>
2024-09-13 17:27:18 +0530

    commited

--- example.py
+++ example.py
@@ -1,3 +1,15 @@
 k = 6
 j = k + 6
 print("aaj khane me kolhapuri paneer milega")
+#author @Rohanchidri
+
+def subtract_numbers(num1, num2):
+	result = num1 - num2
+	return result
+
+
+number1 = 40
+number2 = 18
+subtraction_result = subtract_numbers(number1, number2)
+
+print(f"Subtraction using Function: {subtraction_result}")
