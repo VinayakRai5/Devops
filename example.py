873d71cd6e3ec85bc8e5106950028ef73d8eb8b9
priyeshgupta14 <21bds050@iiitdwd.ac.in>
2024-09-13 17:16:56 +0530

    sunny_paaji

--- example.py
+++ example.py
@@ -1,11 +1,18 @@
 # author @VinayakRai5
 
 print ("Hello Friyesh")
-
+        
 # author @Gacha76
 def sum(a, b):
     return a+b
 
+# author @priyeshgupta14
+
+def dada_chalte_to_mass(no_of_steps):
+    for dada in range(0,no_of_steps):
+        print("Mai nikla gaddi leke eheee {} mod aaya".format(dada))
+
+dada_chalte_to_mass(6)
 k = 6
 j = k + 6
 print("aaj khane me kolhapuri paneer milega")
